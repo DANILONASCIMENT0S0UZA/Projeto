3b8bbb12e8890bf03eed962a719a8aee3d3ae585
DANILO NASCIMENTO SOUZA <danilo.nsouza1@senacsp.edu.br>
2023-06-27 17:15:40 -0300

    Sistema

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    CargoController,
+    ClienteController,
+    EnderecoController,
+    PedidoController,
+    ProdutoController,
+    ProdutoTamanhoController,
+    ProfileController,
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +36,111 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+/**
+ * -----------------------------------
+ * Cargos
+ * -----------------------------------
+ */
+
+ Route::prefix('cargos')
+  ->controller(CargoController::class)
+  ->group(function ()  {
+  Route::get('/','index')->name('cargo.index');
+  Route::get('/novo','create')->name('cargo.create');
+  Route::get('/{id}','show')->name('cargo.show');
+  Route::get('/edit/{id}','edit')->name('cargo.edit');
+  Route::get('/store','store')->name('cargo.store');
+  Route::get('/update','update')->name('cargo.update');
+  Route::get('/destroy','destroy')->name('cargo.destroy');
+ });
+
+ /**
+ * -----------------------------------
+ * Clientes
+ * -----------------------------------
+ */
+
+ Route::prefix('clientes')
+  ->controller(ClienteController::class)
+  ->group(function ()  {
+  Route::get('/','index')->name('cliente.index');
+  Route::get('/novo','create')->name('cliente.create');
+  Route::get('/{id}','show')->name('cliente.show');
+  Route::get('/edit/{id}','edit')->name('cliente.edit');
+  Route::get('/store','store')->name('cliente.store');
+  Route::get('/update','update')->name('cliente.update');
+  Route::get('/destroy','destroy')->name('cliente.destroy');
+ });
+
+ /**
+ * -----------------------------------
+ * Endereço
+ * -----------------------------------
+ */
+
+ Route::prefix('enderecos')
+  ->controller(EnderecoController::class)
+  ->group(function ()  {
+  Route::get('/','index')->name('endereco.index');
+  Route::get('/novo','create')->name('endereco.create');
+  Route::get('/{id}','show')->name('endereco.show');
+  Route::get('/edit/{id}','edit')->name('endereco.edit');
+  Route::get('/store','store')->name('endereco.store');
+  Route::get('/update','update')->name('endereco.update');
+  Route::get('/destroy','destroy')->name('endereco.destroy');
+ });
+
+ /**
+ * -----------------------------------
+ * Pedido
+ * -----------------------------------
+ */
+
+ Route::prefix('pedidos')
+ ->controller(PedidoController::class)
+ ->group(function ()  {
+ Route::get('/','index')->name('pedido.index');
+ Route::get('/novo','create')->name('pedido.create');
+ Route::get('/{id}','show')->name('pedido.show');
+ Route::get('/edit/{id}','edit')->name('pedido.edit');
+ Route::get('/store','store')->name('pedido.store');
+ Route::get('/update','update')->name('pedido.update');
+ Route::get('/destroy','destroy')->name('pedido.destroy');
+ });
+
+ /**
+ * -----------------------------------
+ * Produto
+ * -----------------------------------
+ */
+
+ Route::prefix('produtos')
+  ->controller(ProdutoController::class)
+  ->group(function ()  {
+  Route::get('/','index')->name('produto.index');
+  Route::get('/novo','create')->name('produto.create');
+  Route::get('/{id}','show')->name('produto.show');
+  Route::get('/edit/{id}','edit')->name('produto.edit');
+  Route::get('/store','store')->name('produto.store');
+  Route::get('/update','update')->name('produto.update');
+  Route::get('/destroy','destroy')->name('produto.destroy');
+ });
+
+ /**
+ * -----------------------------------
+ * PrpdutoTamanhos
+ * -----------------------------------
+ */
+
+ Route::prefix('tamanhos')
+ ->controller(ProdutoTamanhoController::class)
+ ->group(function ()  {
+ Route::get('/','index')->name('tamanho.index');
+ Route::get('/novo','create')->name('tamanho.create');
+ Route::get('/{id}','show')->name('tamanho.show');
+ Route::get('/edit/{id}','edit')->name('tamanho.edit');
+ Route::get('/store','store')->name('tamanho.store');
+ Route::get('/update','update')->name('tamanho.update');
+ Route::get('/destroy','destroy')->name('tamanho.destroy');
+ });
 require __DIR__.'/auth.php';
